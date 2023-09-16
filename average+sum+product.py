97ebd15b757c7ffe3ef0d853f85a7ddc07d77184
Houwenhuan <144777344+Houwenhuan@users.noreply.github.com>
2023-09-17 00:26:49 +0800

    Add files via upload

---
+++ average+sum+product.py
@@ -0,0 +1,17 @@
+num1 = int(input('enter the first number:'))
+
+num2 = int(input('enter the second number:'))
+
+num3 = int(input(' enter the third number"'))
+
+sum_0 = num1 + num2 + num3
+
+product = num1 * num2 * num3
+
+average = sum_0 / 3
+
+print('the sum is', sum_0)
+
+print('the product is', product)
+
+print(f'the average is, {average:.2f}')
