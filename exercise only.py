f6fd4c44eb6e0de6272cb5b2248485b9cfdfbecd
Houwenhuan <144777344+Houwenhuan@users.noreply.github.com>
2023-09-19 13:53:00 +0800

    Merge remote-tracking branch 'origin/assignment' into assignment
    # Conflicts:
    #	Kilograms and grams.py

---
+++ exercise only.py
@@ -0,0 +1,59 @@
+# # FISHER
+# # length = float(input('enter the length of a zander in centimeters:'))
+# # length_limit = 42
+# # if length >= length_limit:
+# #     print('Congratulations! You can take away the zander!')
+# # else:
+# #     centimeters_below_the_size_limit = float(length_limit - length)
+# #     print(f'The centimeters below the size limit is {centimeters_below_the_size_limit:.2f} cm')
+# #     print('Please release the fish back into the lake!')
+# # CRUISE
+# # class of a cruise ship and then prints out a written description according to the list below
+# # LUX: upper-deck cabin with a balcony.
+# # A: above the car deck, equipped with a window.
+# # B: windowless cabin above the car deck.
+# # C: windowless cabin below the car deck.
+# # cabin_class = (input('enter the cabin class of a cruise ship(LUX,A,B,C): '))
+# # if cabin_class == 'LUX':
+# #     print('LUX: upper-deck cabin with a balcony.')
+# # elif cabin_class == 'A':
+# #     print('A: above the car deck, equipped with a window.')
+# # elif cabin_class == 'B':
+# #     print('B: windowless cabin above the car deck.')
+# # elif cabin_class == 'C':
+# #     print('C: windowless cabin below the car deck')
+# # else:
+# #     print('error message:Invalid cabin class')
+# # VALUE
+# # A normal hemoglobin value for adult females is between 117-155 g/l.
+# # A normal hemoglobin value for adult males is between 134-167 g/l.
+# # male = float(input('enter your hemoglobin value(g/l) for male:'))
+# # if male < 117:
+# #     print('Your hemoglobin value for male is low')
+# # elif male > 155:
+# #     print('Your hemoglobin value is for male high')
+# # else:
+# #     print('Your hemoglobin value for male is normal')
+#
+# # gender = input('enter your gender(male or female):')
+# # value = float(input('enter your value'))
+# # if gender == 'male':
+# #     if value < float('117'):
+# #         print('Your hemoglobin value for male is low')
+# #     elif float('117') <= value <= float('155'):
+# #         print('Your hemoglobin value is for male is normal')
+# #     else:
+# #         print('Your hemoglobin value for male is high')
+# # if gender == 'female':
+# #     if value < float('134'):
+# #         print('Your hemoglobin value for female is low')
+# #     elif float('134') <= value <= float('167'):
+# #         print('Your hemoglobin value is for female normal')
+# #     else:
+# #         print('Your hemoglobin value for female is high')
+# # LEAP YEAR
+# year = int(input('enter a year'))
+# if year / 4 == an integer:
+#     print(f'The year of  (year) + is a leap year')
+# else:
+#     print(f'The year of float{year:.2f}year is not a leap year')
--- fisher.py
+++ fisher.py
@@ -1,9 +1,10 @@
 # 42及以上才可以放生，42以下要release
+
 length = float(input('enter the length of a zander in centimeters:'))
-size_limit = 42
-if length >= size_limit:
-    print('Congratulations！You can take away the zander')
+length_limit = 42
+if length >= length_limit:
+    print('Congratulations! You can take away the zander!')
 else:
-    below_size_limit = size_limit - length
-    print(f'The centimeter is below the size limit:{below_size_limit:.2f}')
-    print('Release the fish back into the lake!')
+    centimeters_below_the_size_limit = float(length_limit - length)
+    print(f'The centimeters below the size limit is {centimeters_below_the_size_limit:.2f} cm')
+    print('Please release the fish back into the lake!')
