7a3866627790da25440e6af9837ecc41777fa893
Houwenhuan <144777344+Houwenhuan@users.noreply.github.com>
2023-09-19 13:53:00 +0800

    Merge remote-tracking branch 'origin/assignment' into assignment
    # Conflicts:
    #	Kilograms and grams.py

--- hemoglobin value.py
+++ hemoglobin value.py
@@ -1,21 +1,21 @@
 # A normal hemoglobin value for adult females is between 117-155 g/l.
 # A normal hemoglobin value for adult males is between 134-167 g/l.
-
-male = float(input('enter your hemoglobin value(g/l) for male:'))
-if male < 117:
-    print('Your hemoglobin value for male is low')
-elif male > 155:
-    print('Your hemoglobin value is for male high')
-else:
-    print('Your hemoglobin value for male is normal')
-
-female = float(input('enter your hemoglobin value(g/l) for female:'))
-if female < 134:
-    print('Your hemoglobin value for female is low')
-elif male > 167:
-    print('Your hemoglobin value is for female high')
-else:
-    print('Your hemoglobin value for female is normal')
+gender = input('enter your gender(male or female):')
+value = float(input('enter your value'))
+if gender == 'male':
+    if value < float('117'):
+        print('Your hemoglobin value for male is low')
+    elif float('117') <= value <= float('155'):
+        print('Your hemoglobin value is for male is normal')
+    else:
+        print('Your hemoglobin value for male is high')
+if gender == 'female':
+    if value < float('134'):
+        print('Your hemoglobin value for female is low')
+    elif float('134') <= value <= float('167'):
+        print('Your hemoglobin value is for female normal')
+    else:
+        print('Your hemoglobin value for female is high')
 
 
 
